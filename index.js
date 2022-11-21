39c962906eea47e60b681fe72423609f390d7eb0
mhantschk <118595636+mhantschk@users.noreply.github.com>
2022-11-21 22:49:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ solvistemp.prototype = {
     getTemperature: function(callback) {
         this.log('getTemperature');
         this.log(this.xml);
-        needle.get('http://solvis:solvis@192.168.178.80', function(error, response) {
+        needle.get('http://solvis:solvis@solvisremote-576839.qrr9tjqalkqynuff.myfritz.net/', function(error, response) {
 //  if (!error && response.statusCode == 200)
     console.log(error);
     console.log(response.body);
