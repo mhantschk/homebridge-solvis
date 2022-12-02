e33814508b33fcbc5b0e5c2b8f29f3762b9b374f
mhantschk <118595636+mhantschk@users.noreply.github.com>
2022-12-02 20:49:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ solvistemp.prototype = {
                     throw err; // you need to handle error
                }
                //console.log(data);
-               console.log(res.string);
+               console.log(res.getelementsbytagname('data'));
                // data is Buffer instance
                //console.log(data.toString());
           });
